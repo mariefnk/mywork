4905dd768ddfde5e07265c65f1ad6f7de095ffe7
MashaK <kochetygova.m@husky.meu.edu>
2017-04-08 18:24:42 -0400

    Delete useless code

--- maria-code/Javascript/jobsScript.js
+++ maria-code/Javascript/jobsScript.js
@@ -20,34 +20,6 @@ $(document).ready(function () {
     $("#hideContract").click(function(){
         $("#newContract").hide();
     });
-
-
-
-    $('.panel-heading span.clickable').click( function (e) {
-    var $this = $(this);
-    if (!$this.hasClass('panel-collapsed')) {
-        $this.parents('.panel').find('.panel-body').slideUp();
-        $this.addClass('panel-collapsed');
-        $this.find('i').removeClass('glyphicon-minus').addClass('glyphicon-plus');
-    } else {
-        $this.parents('.panel').find('.panel-body').slideDown();
-        $this.removeClass('panel-collapsed');
-        $this.find('i').removeClass('glyphicon-plus').addClass('glyphicon-minus');
-    }
-    });
-
-    $('.panel div.clickable').click(function (e) {
-    var $this = $(this);
-    if (!$this.hasClass('panel-collapsed')) {
-        $this.parents('.panel').find('.panel-body').slideUp();
-        $this.addClass('panel-collapsed');
-        $this.find('i').removeClass('glyphicon-minus').addClass('glyphicon-plus');
-    } else {
-        $this.parents('.panel').find('.panel-body').slideDown();
-        $this.removeClass('panel-collapsed');
-        $this.find('i').removeClass('glyphicon-plus').addClass('glyphicon-minus');
-    }
-    });
 });
 /*
 $(document).unbind().on('click', '.panel-heading span.clickable', function (e) {
